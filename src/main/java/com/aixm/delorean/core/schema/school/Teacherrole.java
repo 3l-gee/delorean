4b9f58c4175aff210b89153a82a008ca7b69e840
3l-gee <raphael.gerth@gmail.com>
2024-10-04 19:54:42 +0200

    added pointdb and pointxml to school

--- src/main/java/com/aixm/delorean/core/schema/school/Teacherrole.java
+++ src/main/java/com/aixm/delorean/core/schema/school/Teacherrole.java
@@ -10,13 +10,10 @@ package com.aixm.delorean.core.schema.school;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.xml.bind.annotation.XmlEnum;
-import jakarta.xml.bind.annotation.XmlEnumValue;
 import jakarta.xml.bind.annotation.XmlType;
 
 
 /**
- * 
- * 
  * <p>Java class for teacherrole</p>.
  * 
  * <p>The following schema fragment specifies the expected content contained within this class.</p>
@@ -37,45 +34,16 @@ import jakarta.xml.bind.annotation.XmlType;
 @Table(name = "teacher_role")
 public enum Teacherrole {
 
-    @XmlEnumValue("math")
-    MATH("math"),
-    @XmlEnumValue("german")
-    GERMAN("german"),
-    @XmlEnumValue("english")
-    ENGLISH("english");
-    private final String value;
+    math,
+    german,
+    english;
 
-    Teacherrole(String v) {
-        value = v;
-    }
-
-    /**
-     * Gets the value associated to the enum constant.
-     * 
-     * @return
-     *     The value linked to the enum.
-     */
     public String value() {
-        return value;
+        return name();
     }
 
-    /**
-     * Gets the enum associated to the value passed as parameter.
-     * 
-     * @param v
-     *     The value to get the enum from.
-     * @return
-     *     The enum which corresponds to the value, if it exists.
-     * @throws IllegalArgumentException
-     *     If no value matches in the enum declaration.
-     */
     public static Teacherrole fromValue(String v) {
-        for (Teacherrole c: Teacherrole.values()) {
-            if (c.value.equals(v)) {
-                return c;
-            }
-        }
-        throw new IllegalArgumentException(v);
+        return valueOf(v);
     }
 
 }
