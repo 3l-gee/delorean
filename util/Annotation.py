479196bd6ecfa2cd816a15348354496af4cf4414
3l-gee <raphael.gerth@gmail.com>
2024-11-14 23:36:57 +0100

    added simple_type capabilites to xjb machinery

--- util/Annotation.py
+++ util/Annotation.py
@@ -13,158 +13,158 @@ class Util:
         return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower().replace("_base_type","").replace("_type","")
     
 class Property:
-    NAME = '<jaxb:property name="aixmName"/>'
-    ELEMENT = '<jaxb:property generateElementProperty="false"/>'
-    NAME_ELEMENT = '<jaxb:property name="aixmName" generateElementProperty="false"/>'
+    name = '<jaxb:property name="aixmName"/>'
+    element = '<jaxb:property generateElementProperty="false"/>'
+    name_element = '<jaxb:property name="aixmName" generateElementProperty="false"/>'
 
 
 class Jaxb:
+
     @staticmethod
-    def COMPLEX(value, at="name"):
+    def schema(value):
+        return f'''<jaxb:bindings schemaLocation="{value}" node="/xs:schema">'''
+
+    @staticmethod
+    def simple(value, at="name"):
+        return f'''<jaxb:bindings node="//xs:simpleType[@{at}='{value}']">'''
+    
+    @staticmethod
+    def complex(value, at="name"):
         return f'''<jaxb:bindings node="//xs:complexType[@{at}='{value}']">'''
 
     @staticmethod
-    def GROUP(value, at="name"):
+    def group(value, at="name"):
         return f'''<jaxb:bindings node="//xs:group[@{at}='{value}']">'''
 
     @staticmethod
-    def ELEMENT(value, at="name"):
+    def element(value, at="name"):
         return f'''<jaxb:bindings node="//xs:element[@{at}='{value}']">'''
 
     @staticmethod
-    def ATTRIBUTE(value, at="name"):
+    def attribute(value, at="name"):
         return f'''<jaxb:bindings node="//xs:attribute[@{at}='{value}']">'''
 
     @staticmethod
-    def ENUM_START(value):
+    def enum_start(value):
         return f'''<jaxb:typesafeEnumClass name="{value}">'''
 
     @staticmethod
-    def ENUM_MEMBER(name, value):
+    def enum_member(name, value):
         return f'''<jaxb:typesafeEnumMember name="{name}" value="{value}"/>'''
 
-    ENUM_END = '</jaxb:typesafeEnumClass>'
-    END = '</jaxb:bindings>'
+    enum_end = '</jaxb:typesafeEnumClass>'
+    end = '</jaxb:bindings>'
 
     PROPERTY = Property
 
 class Annox:
     @staticmethod
-    def CLASS(annotation, add=True):
-        if add == True:
-            return f'''<annox:annotate target="class">{annotation}</annox:annotate>'''
-        elif add == False:
-            return f'''<annox:removeAnnotation target="class">{annotation}</annox:annotate>'''
-        else : 
-            ValueError("add must be True or False")
+    def class_add(annotation):
+        return f'''<annox:annotate target="class">{annotation}</annox:annotate>'''
 
     @staticmethod
-    def FIELD(annotation, add=True):
-        if add == True:
-            return f'''<annox:annotate target="field">{annotation}</annox:annotate>'''
-        elif add == False:
-            return f'''<annox:removeAnnotation target="class">{annotation}</annox:annotate>'''
-        else : 
-            ValueError("add must be True or False")
+    def class_remove(annotation):
+        return f'''<annox:removeAnnotation target="class">{annotation}</annox:annotate>'''
+
+    @staticmethod
+    def field_add(annotation):
+        return f'''<annox:annotate target="field">{annotation}</annox:annotate>'''
+
+    def field_remove(annotation):
+        return f'''<annox:removeAnnotation target="field">{annotation}</annox:annotate>'''
 
 class Tag:
-    XS_NAMESPACE = "{http://www.w3.org/2001/XMLSchema}"
+    xs_namespace = "{http://www.w3.org/2001/XMLSchema}"
     
-    IMPORT = XS_NAMESPACE + "import"
-    ANNOTATION = XS_NAMESPACE + "annotation"
-    INCLUDE = XS_NAMESPACE + "include"
-    ELEMENT = XS_NAMESPACE + "element"
-    ATTRIBUTE = XS_NAMESPACE + "attribute"
-    COMPLEX_TYPE = XS_NAMESPACE + "complexType"
-    SIMPLE_TYPE = XS_NAMESPACE + "simpleType"
-    GROUP = XS_NAMESPACE + "group"
-    ALL = XS_NAMESPACE + "all"
-    ANY = XS_NAMESPACE + "any"
-    ANY_ATTRIBUTE = XS_NAMESPACE + "anyAttribute"
-    CHOICE = XS_NAMESPACE + "choice"
-    SEQUENCE = XS_NAMESPACE + "sequence"
-    RESTRICTION = XS_NAMESPACE + "restriction"
-    EXTENSION = XS_NAMESPACE + "extension"
-    KEY = XS_NAMESPACE + "key"
-    KEYREF = XS_NAMESPACE + "keyref"
-    UNIQUE = XS_NAMESPACE + "unique"
-    ATTRIBUTE_GROUP = XS_NAMESPACE + "attributeGroup"
-    COMPLEX_CONTENT = XS_NAMESPACE + "complexContent"
-    SIMPLE_CONTENT = XS_NAMESPACE + "simpleContent"
-    NOTATION = XS_NAMESPACE + "notation"
-    DOCUMENTATION = XS_NAMESPACE + "documentation"
-    APPINFO = XS_NAMESPACE + "appinfo"
+    import_ = xs_namespace + "import"
+    annotation = xs_namespace + "annotation"
+    include = xs_namespace + "include"
+    element = xs_namespace + "element"
+    attribute = xs_namespace + "attribute"
+    complex_type = xs_namespace + "complexType"
+    simple_type = xs_namespace + "simpleType"
+    group = xs_namespace + "group"
+    all = xs_namespace + "all"
+    any = xs_namespace + "any"
+    any_attribute = xs_namespace + "anyAttribute"
+    choice = xs_namespace + "choice"
+    sequence = xs_namespace + "sequence"
+    restriction = xs_namespace + "restriction"
+    extension = xs_namespace + "extension"
+    key = xs_namespace + "key"
+    keyref = xs_namespace + "keyref"
+    unique = xs_namespace + "unique"
+    attribute_group = xs_namespace + "attributeGroup"
+    complex_content = xs_namespace + "complexContent"
+    simple_content = xs_namespace + "simpleContent"
+    notation = xs_namespace + "notation"
+    documentation = xs_namespace + "documentation"
+    appinfo = xs_namespace + "appinfo"
 
 class Xml:
-    TAG = Tag
-    TRANSIENT = '@jakarta.xml.bind.annotation.XmlTransient'
+    tag = Tag
+    transient = '@jakarta.xml.bind.annotation.XmlTransient'
     @staticmethod
-    def TYPE(name, propOrder):
+    def type(name, propOrder):
         return f'@jakarta.xml.bind.annotation.XmlType(name = "{name}", propOrder = {{"{propOrder}"}})'
 
 class Relation:
     @staticmethod
-    def ONE_TO_ONE(cascade="CascadeType.ALL", fetch="FetchType.EAGER", orphanRemoval=False):   
-        if orphanRemoval==False:
-            return f'@jakarta.persistence.OneToOne(cascade={cascade}, fetch={fetch})'
-        
-        else :
-            return f'@jakarta.persistence.OneToOne(cascade={cascade}, fetch={fetch}, orphanRemoval={str(orphanRemoval).lower()})'
+    def one_to_one(cascade="CascadeType.ALL", fetch="FetchType.EAGER"):   
+        return f'@jakarta.persistence.OneToOne(cascade={cascade}, fetch={fetch})'
+    
+    @staticmethod
+    def one_to_one_with_orphan_removal(cascade="CascadeType.ALL", fetch="FetchType.EAGER", orphanRemoval=True):   
+        return f'@jakarta.persistence.OneToOne(cascade={cascade}, fetch={fetch}, orphanRemoval={str(orphanRemoval).lower()})'
 
     @staticmethod
-    def ONE_TO_MANY(cascade="CascadeType.ALL", fetch="FetchType.EAGER", orphanRemoval=False):   
-        if orphanRemoval==False:
-            return f'@jakarta.persistence.OneToMany(cascade={cascade}, fetch={fetch})'
-        
-        else :
-            return f'@jakarta.persistence.OneToMany(cascade={cascade}, fetch={fetch}, orphanRemoval={str(orphanRemoval).lower()})'
+    def one_to_many(cascade="CascadeType.ALL", fetch="FetchType.EAGER"):   
+        return f'@jakarta.persistence.OneToMany(cascade={cascade}, fetch={fetch})'
     
     @staticmethod
-    def MANY_TO_ONE(cascade="CascadeType.ALL", fetch="FetchType.EAGER", orphanRemoval=False):  
-        if orphanRemoval==False:
-            return f'@jakarta.persistence.ManyToOne(cascade={cascade}, fetch={fetch})'
-        
-        else :
-            return f'@jakarta.persistence.ManyToOne(cascade={cascade}, fetch={fetch}, orphanRemoval={str(orphanRemoval).lower()})'
+    def one_to_many_with_orphan_removal(cascade="CascadeType.ALL", fetch="FetchType.EAGER", orphanRemoval=True):   
+        return f'@jakarta.persistence.OneToMany(cascade={cascade}, fetch={fetch}, orphanRemoval={str(orphanRemoval).lower()})'
     
     @staticmethod
-    def MANY_TO_MANY(cascade="CascadeType.ALL", fetch="FetchType.EAGER", orphanRemoval=False):  
-        if orphanRemoval==False:
-            return f'@jakarta.persistence.ManyToMany(cascade={cascade}, fetch={fetch})'
-        
-        else :
-            return f'@jakarta.persistence.ManyToMany(cascade={cascade}, fetch={fetch}, orphanRemoval={str(orphanRemoval).lower()})'
+    def many_to_one(cascade="CascadeType.ALL", fetch="FetchType.EAGER"):  
+        return f'@jakarta.persistence.ManyToOne(cascade={cascade}, fetch={fetch})'
+
+    @staticmethod
+    def many_to_one_with_orphan_removal(cascade="CascadeType.ALL", fetch="FetchType.EAGER", orphanRemoval=True):  
+        return f'@jakarta.persistence.ManyToOne(cascade={cascade}, fetch={fetch}, orphanRemoval={str(orphanRemoval).lower()})'
     
     @staticmethod
-    def JOIN_COLUMN(name, referencedColumnName="id"):
+    def many_to_many(cascade="CascadeType.ALL", fetch="FetchType.EAGER"):  
+        return f'@jakarta.persistence.ManyToMany(cascade={cascade}, fetch={fetch})'
+    
+    @staticmethod
+    def many_to_many_with_orphan_removal(cascade="CascadeType.ALL", fetch="FetchType.EAGER", orphanRemoval=True):  
+        return f'@jakarta.persistence.ManyToMany(cascade={cascade}, fetch={fetch}, orphanRemoval={str(orphanRemoval).lower()})'
+    
+    @staticmethod
+    def join_column(name, referencedColumnName="id"):
         return f'@jakarta.persistence.JoinColumn(name="{Util.snake_case(name)}_id", referencedColumnName={referencedColumnName})'
-    
-   
 
 class jpa:
     
-    ENTITY = '@jakarta.persistence.Entity'
-    ID = '@jakarta.persistence.Id'
-    TRANSIENT = '@jakarta.persistence.Transient'
-    EMBEDDABLE = '@jakarta.persistence.Embeddable'
-    EMBEDDED = '@jakarta.persistence.Embedded'
+    entity = '@jakarta.persistence.Entity'
+    id = '@jakarta.persistence.Id'
+    transient = '@jakarta.persistence.Transient'
+    embeddable = '@jakarta.persistence.Embeddable'
+    embedded = '@jakarta.persistence.Embedded'
 
     @staticmethod
-    def COLUMN(name, columnDefinition=None, nullable=True, 	unique=False):
-        if columnDefinition is None:
-            return f'@jakarta.persistence.Column(name = "{Util.snake_case(name)}", nullable = {nullable}, unique = {unique})'
+    def column(name, nullable=False, unique=False):
+        return f'@jakarta.persistence.Column(name = "{Util.snake_case(name)}", nullable = {nullable}, unique = {unique})'
 
-        else:
-            return  f'@jakarta.persistence.Column(name = "{Util.snake_case(name)}", columnDefinition = "{columnDefinition}",  nullable = {nullable}, unique = {unique})'
+    @staticmethod
+    def column_with_definition(name, columnDefinition, nullable=False, unique=False):
+        return f'@jakarta.persistence.Column(name = "{Util.snake_case(name)}", columnDefinition = "{columnDefinition}", nullable = {nullable}, unique = {unique})'
         
     @staticmethod
-    def TABLE(name, schema=None):
-        if schema is None:
-            return f'@jakarta.persistence.Table(name = "{Util.snake_case(name)}")'
-    
-        else:
-            return f'@jakarta.persistence.Table(name = "{Util.snake_case(name)}", schema = "{schema}")'
+    def table(name, schema):
+        return f'@jakarta.persistence.Table(name = "{Util.snake_case(name)}", schema = "{schema}")'
 
     @staticmethod
-    def ENUMERATED(value="STRING"):
+    def enumerated(value="STRING"):
         return f'@jakarta.persistence.Enumerated(jakarta.persistence.EnumType.{value})'
