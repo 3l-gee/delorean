c04126b8949f81b13183cc41981e509590701938
3l-gee <raphael.gerth@gmail.com>
2024-12-05 00:06:21 +0100

    first sucessfull load / extract + worked on timeslice

---
+++ src/main/java/com/aixm/delorean/core/adapter/type/time/AixmTimeSliceType.java
@@ -0,0 +1,14 @@
+package com.aixm.delorean.core.adapter.type.time;
+
+import javax.xml.datatype.Duration;
+
+import com.aixm.delorean.core.schema.a5_1_1.org.gml.TimeInstantPropertyType;
+import com.aixm.delorean.core.schema.a5_1_1.org.gml.TimeIntervalLengthType;
+import com.aixm.delorean.core.schema.a5_1_1.org.gml.TimePositionType;
+
+public class AixmTimeSliceType {
+
+    protected TimePositionType begin;
+    protected TimePositionType end;
+
+}
