3f4ed6a1cd01a8cc1ed370e38bdad5894c8e309d
3l-gee <raphael.gerth@gmail.com>
2025-05-26 23:18:52 +0200

    working solution for associated feature identification

--- qgis/pygis/generic_feature_selector.py
+++ qgis/pygis/generic_feature_selector.py
@@ -26,17 +26,13 @@ QTimer.singleShot(2000, clear_filter)
 
 
 from qgis.utils import iface
-from qgis.core import QgsPointXY, QgsProject
-from qgis.gui import QgsMapToolIdentify
+from qgis.core import QgsProject
 
 layer = QgsProject.instance().mapLayersByName('airportheliport_publisher_view')[0]
 uuid = '[%regexp_substr(airportheliport_href,'([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})')%]'
-geom = 
 feature = next(layer.getFeatures(f'"identifier" = \'{uuid}\''), None)
 
 if feature:
-    point = feature.geometry().centroid().asPoint()
-    identify_tool = QgsMapToolIdentify(iface.mapCanvas())
-    results = identify_tool.identify(QgsPointXY(point), [layer], QgsMapToolIdentify.TopDownStopAtFirst)
-    iface.identifyResultsDialog().addResults(results)
-    iface.identifyResultsDialog().show()
+    print(feature)
+    iface.openFeatureForm(layer, feature, False)
+    
