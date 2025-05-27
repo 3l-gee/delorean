c01e65158c36d5ed55cac152c61b16a558b95fc6
3l-gee <raphael.gerth@gmail.com>
2025-05-27 23:27:05 +0200

    template, layer

---
+++ src/main/java/com/aixm/delorean/core/qgis/QgisTemplateConfig.java
@@ -0,0 +1,16 @@
+package com.aixm.delorean.core.qgis;
+
+public enum QgisConfig {
+    
+
+    A5_1(
+        "delorean aixm 5.1 publish",
+        "delorean aixm 5.1 edit"
+    )
+
+    String publisherTitle;
+    String editorTitle;
+    String metadata;
+
+
+}
