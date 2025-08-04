bbf3733c875b62e856329cd55dfc6e245c757c64
3l-gee <raphael.gerth@gmail.com>
2025-08-04 23:33:12 +0200

    improved annotation clarity, fixed linking issue, worked on timesheet

--- viewgen/version/a5_1/sql/ContactInformationPropertyType.sql
+++
@@ -1,7 +0,0 @@
--- Snowflake SQL : ContactInformationPropertyType 
-
-DROP MATERIALIZED VIEW IF EXISTS shared.contactinformation_view CASCADE;
-
-CREATE MATERIALIZED VIEW shared.contactinformation_view AS
-SELECT 
-0 AS id;
