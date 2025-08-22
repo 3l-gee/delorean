788ddcef4d20b6dfdd3bf470c07f27fdefb8373d
Raphael Gerth <124341972+3l-gee@users.noreply.github.com>
2025-08-22 09:39:52 +0200

    Update vite.config.js
    Fix : use root for custom domain

--- website/vite.config.js
+++ website/vite.config.js
@@ -4,6 +4,6 @@ import tailwind from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/delorean/',  
+  base: '/',  
   plugins: [preact(), tailwind()],
 })
